1195d453851445c66e1dd6f3f3676baf6d756da8
Unknown <sylvi.bluemochi@gmail.com>
2018-11-14 14:15:25 +0700

    tambah Login page

---
+++ app/src/main/java/com/example/thomas/penjuallpay/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.thomas.penjuallpay;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    public Button btnLoginLogin;
+    public Button btnLoginRegister;
+
+    private static long back_pressed ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiy_login);
+
+        btnLoginLogin = (Button) findViewById(R.id.btnLoginLogin);
+        btnLoginRegister = (Button) findViewById(R.id.btnLoginRegister);
+
+        btnLoginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //LOGIN sementara (tanpa pengecekan nomor&password)
+        btnLoginLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (back_pressed + 2000 > System.currentTimeMillis()){
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            startActivity(intent);
+            finish();
+        } else{
+            Toast.makeText(this, "Press once again to exit",Toast.LENGTH_SHORT).show();
+        }
+        back_pressed = System.currentTimeMillis();
+    }
+}
--- app/src/main/java/com/example/thomas/penjuallpay/SplashPageActivity.java
+++ app/src/main/java/com/example/thomas/penjuallpay/SplashPageActivity.java
@@ -17,7 +17,7 @@ public class SplashPageActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashPageActivity.this, MainActivity.class);
+                Intent homeIntent = new Intent(SplashPageActivity.this, LoginActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
