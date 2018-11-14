55c02b2fe81290678b8b3d70a4f9585889188e48
thomas <thomas.widiarya@ti.ukdw.ac.id>
2018-11-14 15:33:47 +0700

    form transaksi

--- app/src/main/java/com/example/thomas/penjuallpay/TransaksiActivity.java
+++ app/src/main/java/com/example/thomas/penjuallpay/TransaksiActivity.java
@@ -3,8 +3,10 @@ package com.example.thomas.penjuallpay;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class TransaksiActivity extends AppCompatActivity {
 
@@ -12,6 +14,27 @@ public class TransaksiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaksi);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar); // get the reference of Toolbar
+        setSupportActionBar(toolbar); // Setting/replace toolbar as the ActionBar
+        getSupportActionBar().setDisplayShowTitleEnabled(true); // hide the current title from the Toolbar
+        // implement setNavigationOnClickListener event
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            // add code here that execute on click of navigation button
+                //Intent intent1 = new Intent(this,MainActivity.class);
+                //startActivity(intent1);
+                back();
+            }
+        });
+    }
+
+    public void back(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
+        this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
     public void toQR(View v){
