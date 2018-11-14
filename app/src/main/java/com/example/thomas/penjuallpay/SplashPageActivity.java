98238afdb9be450fcbce982575275e5c7f8a26d6
Unknown <sylvi.bluemochi@gmail.com>
2018-11-14 13:48:16 +0700

    tambah splash page

---
+++ app/src/main/java/com/example/thomas/penjuallpay/SplashPageActivity.java
@@ -0,0 +1,26 @@
+package com.example.thomas.penjuallpay;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashPageActivity extends AppCompatActivity {
+    private  static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashPageActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
