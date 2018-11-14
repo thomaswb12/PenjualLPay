493a65af0cece22a0c8ceade31d1f34797910c41
Unknown <sylvi.bluemochi@gmail.com>
2018-11-14 15:35:14 +0700

    uodate enterpin page

--- app/src/main/java/com/example/thomas/penjuallpay/WithdrawActivity.java
+++ app/src/main/java/com/example/thomas/penjuallpay/WithdrawActivity.java
@@ -1,7 +1,9 @@
 package com.example.thomas.penjuallpay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -13,5 +15,15 @@ public class WithdrawActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_withdraw);
+    }
+
+    public void toEnterPin(View v){
+        Intent intent = new Intent(WithdrawActivity.this, EnterpinActivity.class);
+        startActivity(intent);
+    }
+
+    public void toMain(View v){
+        Intent intent = new Intent(WithdrawActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
