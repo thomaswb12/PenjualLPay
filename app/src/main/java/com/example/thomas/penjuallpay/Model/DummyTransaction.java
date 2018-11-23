b7d3773ce2fd9db1a623f6331887dd944d6ddc9b
Unknown <sylvi.bluemochi@gmail.com>
2018-11-23 20:14:34 +0700

    dsas

--- app/src/main/java/com/example/thomas/penjuallpay/Model/DummyTransaction.java
+++ app/src/main/java/com/example/thomas/penjuallpay/Model/DummyTransaction.java
@@ -4,36 +4,33 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class DummyTransaction {
-    private String idTransaksi;
-    private String seller_UID;
     private String telp_pembeli;
-    private int harga;
+    private int total;
     private String waktu;
     private String nama_toko;
     private String image_toko;
 
-    FirebaseDatabase database =  FirebaseDatabase.getInstance();
-    DatabaseReference mRef;
-
-    public DummyTransaction(int harga, String idTransaksi, String seller_UID){
-        this.harga = harga;
-        this.idTransaksi = idTransaksi;
-        this.seller_UID = seller_UID;
-
-        mRef =  database.getReference().child("transaksi").child("dummy").child(idTransaksi);
-
-        getSellerInfo();
-        writeToDatabase();
+    //constructor
+    public DummyTransaction(int total, String nama_toko){
+        this.total = total;
+        this.nama_toko = nama_toko;
     }
 
-    public void getSellerInfo(){
-        //nantinya:
-            //User user; -> pake kelas user
-            //nama_toko = user.getNamaToko();
+
+    //getter setter
+    public String getNama_toko() {
+        return nama_toko;
     }
 
-    public  void writeToDatabase(){
-        mRef.child("total").setValue(harga);
-        //setValue nama_toko, dll juga
+    public int getTotal() {
+        return total;
+    }
+
+    public void setNama_toko(String nama_toko) {
+        this.nama_toko = nama_toko;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
     }
 }
